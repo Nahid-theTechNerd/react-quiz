8fea71bf695971b32154c0ddd10c4a10cfd72cca
Nahid Hasan <nahid.thetechnerd@gmail.com>
2022-08-08 18:11:02 +0600

    layout completed in lesson4

---
+++ src/Components/video.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function video() {
+  return (
+    <div>
+      <a href="quiz.html">
+        <div class="video">
+          <img src="./images/3.jpg" alt="" />
+          <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+          <div class="qmeta">
+            <p>10 Questions</p>
+            <p>Score : Not taken yet</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}

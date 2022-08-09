9e8b223f8819895b363dc189f4f3eacdc8bb5463
Nahid Hasan <nahid.thetechnerd@gmail.com>
2022-08-09 12:17:17 +0600

    Login page complete after lesson5 (assignment)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,14 @@
 import "../styles/App.css";
 import Layout from "./Layout";
+import Login from "./Pages/Login";
 // import Home from "./Pages/Home";
-import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         {/* Write the main component name here */}
-        <SignUp></SignUp>
+        <Login></Login>
       </Layout>
     </div>
   );
--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,5 +1,35 @@
 import React from "react";
+import illustration from "../../assets/images/login.svg";
+import classes from "../../styles/SignUp.module.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <h1>Login to your account</h1>
+      <div className="column">
+        <div className={classes.illustration}>
+          <img src={illustration} alt="Login" />
+        </div>
+        <form className={`${classes.login} ${classes.form}`} action="#">
+          <div className={classes.textInput}>
+            <input type="text" placeholder="Enter email" />
+            <span className="material-icons-outlined"> alternate_email </span>
+          </div>
+
+          <div className={classes.textInput}>
+            <input type="password" placeholder="Enter password" />
+            <span className="material-icons-outlined"> lock </span>
+          </div>
+
+          <button className={classes.button}>
+            <span>Submit now</span>
+          </button>
+
+          <div className="info">
+            Don't have an account? <a href="signup.html">Signup</a> instead.
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }

d71b11cd0264982c130d25590fcd2a5a1083bdad
Nahid Hasan <nahid.thetechnerd@gmail.com>
2022-08-09 11:28:33 +0600

    signup page complete lesson5

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,17 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Home from "./Pages/Home";
+// import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Home></Home>
+        {/* Write the main component name here */}
+        <SignUp></SignUp>
       </Layout>
     </div>
   );
 }
 
 export default App;
-
